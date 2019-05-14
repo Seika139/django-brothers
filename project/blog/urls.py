6b8dff6b4956d48f764ad654af2fd4095c6213e8
Seika139 <tcoanzu@gmail.com>
2019-05-14 20:21:25 +0900

    correct errors 2

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-     path('detail/<int:blog_id>/', views.detail, name='detail'),
+    path('detail/<int:blog_id>/', views.detail, name='detail'),
 ]
