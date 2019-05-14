3aaee18d4adcf057a2994678a790b7cf6e592cb6
Seika139 <tcoanzu@gmail.com>
2019-05-14 20:12:09 +0900

    トップページにブログを表示してみよう

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
 def index(request):
-    return render(request, 'blog/index.html')
+    blog = Blog.objects.order_by('-created_datetime')
+    return render(request, 'blog/index.html', {'blog': blog})
