4c8b581a1cbc8b2fb3f6f8d0fa13a6fe33ca9186
Seika139 <tcoanzu@gmail.com>
2019-05-14 20:15:48 +0900

    チュートリアル終了

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+     path('detail/<int:blog_id>/', views.detail, name='detail'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -4,3 +4,7 @@ from .models import Blog
 def index(request):
     blog = Blog.objects.order_by('-created_datetime')
     return render(request, 'blog/index.html', {'blog': blog})
+
+def detail(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog})
